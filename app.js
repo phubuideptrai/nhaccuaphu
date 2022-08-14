b4776160fc96ba37a0cfb4864fd7b10145d1fc88
Bùi Thanh Phú <74527830+phubuideptrai@users.noreply.github.com>
2022-08-14 20:39:27 +0700

    init

---
+++ app.js
@@ -0,0 +1,700 @@
+const data = [
+    {
+        song: 'mama boy',
+        singer: 'AMME',
+        avatar: './images/01.jpg',
+        mp3: './music/01.mp3'
+    },
+    {
+        song: 'vì mẹ anh bắt chia tay',
+        singer: 'miu lê, karik',
+        avatar: './images/02.jpg',
+        mp3: './music/02.mp3'
+    },
+    {
+        song: 'Shay Nắnggg',
+        singer: 'AMME, obito',
+        avatar: './images/03.jpg',
+        mp3: './music/03.mp3'
+    },
+    {
+        song: 'lời đường mật',
+        singer: 'lyly, hiếu thứ hai',
+        avatar: './images/04.jpg',
+        mp3: './music/04.mp3'
+    },
+    {
+        song: 'một ngàn nỗi đau',
+        singer: 'văn mai hương, hứa kim tuyền',
+        avatar: './images/05.jpg',
+        mp3: './music/05.mp3'
+    },
+    {
+        song: 'hai mươi hai',
+        singer: 'AMME, hứa kim tuyền',
+        avatar: './images/06.jpg',
+        mp3: './music/06.mp3'
+    },
+    {
+        song: 'bao tiền một mớ bình yên',
+        singer: '14 casper, bon',
+        avatar: './images/07.jpg',
+        mp3: './music/07.mp3'
+    },
+    {
+        song: 'tháng tư là lời nói dối của em',
+        singer: 'hà anh tuấn',
+        avatar: './images/08.jpg',
+        mp3: './music/08.mp3'
+    },
+    {
+        song: 'những gì anh nói',
+        singer: 'BOZITT',
+        avatar: './images/09.jpg',
+        mp3: './music/09.mp3'
+    },
+    {
+        song: 'đừng khóc một mình (Piano Version)',
+        singer: 'Quang Hùng MasterD',
+        avatar: './images/10.jpg',
+        mp3: './music/10.mp3'
+    },
+    {
+        song: 'Lỡ say bye là bye',
+        singer: 'Lemese, Changg',
+        avatar: './images/11.jpg',
+        mp3: './music/11.mp3'
+    },
+    {
+        song: 'duyên duyên số số',
+        singer: 'Du uyên',
+        avatar: './images/12.jpg',
+        mp3: './music/12.mp3'
+    },
+    {
+        song: 'Đã lỡ yêu em nhiều',
+        singer: 'JustaTee',
+        avatar: './images/13.jpg',
+        mp3: './music/13.mp3'
+    },
+    {
+        song: 'Chạm đáy nỗi đau (Piano Version)',
+        singer: 'Erik',
+        avatar: './images/14.jpg',
+        mp3: './music/14.mp3'
+    },
+    {
+        song: 'Giữ lấy làm gì',
+        singer: 'MONSTAR',
+        avatar: './images/15.jpg',
+        mp3: './music/15.mp3'
+    },
+    {
+        song: 'mình chia tay đi',
+        singer: 'Erik',
+        avatar: './images/16.jpg',
+        mp3: './music/16.mp3'
+    },
+    {
+        song: 'Về phía mưa',
+        singer: 'Thế bảo',
+        avatar: './images/17.jpg',
+        mp3: './music/17.mp3'
+    },
+    {
+        song: 'Em không sai chúng ta sai',
+        singer: 'Erik',
+        avatar: './images/18.jpg',
+        mp3: './music/18.mp3'
+    },
+    {
+        song: 'Hết thương cạn nhớ',
+        singer: 'Đức Phúc',
+        avatar: './images/19.jpg',
+        mp3: './music/19.mp3'
+    },
+    {
+        song: 'Gặp nhưng không ở lại',
+        singer: 'Hiền Hồ',
+        avatar: './images/20.jpg',
+        mp3: './music/20.mp3'
+    },
+    {
+        song: 'Chẳng thể tìm được em',
+        singer: 'Phúc Xp',
+        avatar: './images/21.jpg',
+        mp3: './music/21.mp3'
+    },
+    {
+        song: 'Anh nhà ở đâu thế (Solo version)',
+        singer: 'AMEE',
+        avatar: './images/22.jpg',
+        mp3: './music/22.mp3'
+    },
+    {
+        song: 'Trái tim em cũng biết đau',
+        singer: 'Mr Siro',
+        avatar: './images/23.jpg',
+        mp3: './music/23.mp3'
+    },
+    {
+        song: 'Đen đá không đường',
+        singer: 'AMEE',
+        avatar: './images/24.jpg',
+        mp3: './music/24.mp3'
+    },
+    {
+        song: 'Trời giấu trời mang đi',
+        singer: 'AMEE',
+        avatar: './images/25.jpg',
+        mp3: './music/25.mp3'
+    },
+    {
+        song: 'Chờ đợi có đáng sợ (Piano Version)',
+        singer: 'Andiez',
+        avatar: './images/26.jpg',
+        mp3: './music/26.mp3'
+    },
+    {
+        song: 'Nghe như tình yêu',
+        singer: 'HIEUTHUHAI',
+        avatar: './images/27.jpg',
+        mp3: './music/27.mp3'
+    },
+    {
+        song: 'Thích em hơi nhiều',
+        singer: 'Wren Evans',
+        avatar: './images/28.jpg',
+        mp3: './music/28.mp3'
+    },
+    {
+        song: 'Anh đánh rơi người yêu này',
+        singer: 'AMEE',
+        avatar: './images/29.jpg',
+        mp3: './music/29.mp3'
+    },
+    {
+        song: 'Không sao mà em đây rồi',
+        singer: 'Suni Hạ Linh',
+        avatar: './images/30.jpg',
+        mp3: './music/30.mp3'
+    },
+    {
+        song: 'Hạnh phúc bỏ rơi em',
+        singer: 'Hương Ly',
+        avatar: './images/31.jpg',
+        mp3: './music/31.mp3'
+    },
+    {
+        song: 'Răng khôn',
+        singer: 'Phí phương anh',
+        avatar: './images/32.jpg',
+        mp3: './music/32.mp3'
+    },
+    {
+        song: 'Suýt nữa thì',
+        singer: 'Andiez',
+        avatar: './images/33.jpg',
+        mp3: './music/33.mp3'
+    },
+    {
+        song: 'Về với anh đi',
+        singer: 'Andiez',
+        avatar: './images/34.jpg',
+        mp3: './music/34.mp3'
+    },
+    {
+        song: 'Cứ yêu đi',
+        singer: 'Đức Phúc, Hoà minzy',
+        avatar: './images/35.jpg',
+        mp3: './music/35.mp3'
+    },
+    {
+        song: 'Em gái mưa (Piano Version)',
+        singer: 'Mr Siro',
+        avatar: './images/36.jpg',
+        mp3: './music/36.mp3'
+    },
+    {
+        song: 'Cả một trời thương nhớ',
+        singer: 'Noo phước thịnh',
+        avatar: './images/37.jpg',
+        mp3: './music/37.mp3'
+    },
+    {
+        song: 'Ai chung tình được mãi',
+        singer: 'Trung Quân',
+        avatar: './images/38.jpg',
+        mp3: './music/38.mp3'
+    },
+    {
+        song: 'Sài gòn hôm nay mưa',
+        singer: 'Hoàng duyên, hứa kim tuyền',
+        avatar: './images/39.jpg',
+        mp3: './music/39.mp3'
+    },
+    {
+        song: 'Ít nhưng dài lâu',
+        singer: 'Nam em',
+        avatar: './images/40.jpg',
+        mp3: './music/40.mp3'
+    },
+    {
+        song: 'Vẫn nhớ',
+        singer: 'Soobin Hoàng sơn',
+        avatar: './images/41.jpg',
+        mp3: './music/41.mp3'
+    },
+    {
+        song: '1 phút',
+        singer: 'Andiez',
+        avatar: './images/42.jpg',
+        mp3: './music/42.mp3'
+    },
+    {
+        song: 'Ngày không có em (Piano version)',
+        singer: 'ThịnK',
+        avatar: './images/43.jpg',
+        mp3: './music/43.mp3'
+    },
+    {
+        song: 'Đường một chiều',
+        singer: 'Huỳnh Tú',
+        avatar: './images/44.jpg',
+        mp3: './music/44.mp3'
+    },
+    {
+        song: 'Màu nước mắt',
+        singer: 'Erik',
+        avatar: './images/45.jpg',
+        mp3: './music/45.mp3'
+    },
+    {
+        song: 'Cánh đồng yêu thương (Mr Siro)',
+        singer: 'Mr siro',
+        avatar: './images/46.jpg',
+        mp3: './music/46.mp3'
+    },
+    {
+        song: 'Dằm trong tim',
+        singer: 'Suni hạ linh, TDK',
+        avatar: './images/47.jpg',
+        mp3: './music/47.mp3'
+    },
+    {
+        song: 'Cánh hoa tàn',
+        singer: 'Hương Tràm',
+        avatar: './images/48.jpg',
+        mp3: './music/48.mp3'
+    },
+    {
+        song: 'Một bước yêu vạn dặm đau',
+        singer: 'Mr Siro',
+        avatar: './images/49.jpg',
+        mp3: './music/49.mp3'
+    },
+    {
+        song: 'Trót yêu',
+        singer: 'Trung Quân',
+        avatar: './images/50.jpg',
+        mp3: './music/50.mp3'
+    },
+    {
+        song: 'Dù sai em vẫn yêu anh',
+        singer: 'Hạnh Sino',
+        avatar: './images/51.jpg',
+        mp3: './music/51.mp3'
+    },
+    {
+        song: 'Em ngày xưa khác rồi',
+        singer: 'Hiền Hồ',
+        avatar: './images/52.jpg',
+        mp3: './music/52.mp3'
+    },
+    {
+        song: 'Sai người sai thời điểm',
+        singer: 'Thanh hưng',
+        avatar: './images/53.jpg',
+        mp3: './music/53.mp3'
+    },
+    {
+        song: 'Rời bỏ',
+        singer: 'Hoà minzy',
+        avatar: './images/54.jpg',
+        mp3: './music/54.mp3'
+    },
+    {
+        song: 'Rồi người thương cũng hoá người dưng',
+        singer: 'Hiền hồ',
+        avatar: './images/55.jpg',
+        mp3: './music/55.mp3'
+    },
+    {
+        song: 'Phải thật hạnh phúc',
+        singer: 'Liz Kim Cương',
+        avatar: './images/56.jpg',
+        mp3: './music/56.mp3'
+    },
+    {
+        song: 'Yêu thương ngày đó',
+        singer: 'Soobin Hoàng sơn',
+        avatar: './images/57.jpg',
+        mp3: './music/57.mp3'
+    },
+    {
+        song: 'Đừng ai nhắc về anh ấy (Piano version)',
+        singer: 'Mr siro',
+        avatar: './images/58.jpg',
+        mp3: './music/58.mp3'
+    },
+    {
+        song: 'Duyên Mình lỡ',
+        singer: 'Hương tràm',
+        avatar: './images/59.jpg',
+        mp3: './music/59.mp3'
+    },
+    {
+        song: 'Sao em nỡ',
+        singer: 'Jaykii, Hiền hồ',
+        avatar: './images/60.jpg',
+        mp3: './music/60.mp3'
+    },
+    {
+        song: 'Đừng như thói quen',
+        singer: 'JayKii',
+        avatar: './images/61.jpg',
+        mp3: './music/61.mp3'
+    },
+    {
+        song: 'Chỉ là muốn nói',
+        singer: 'Khải',
+        avatar: './images/62.jpg',
+        mp3: './music/62.mp3'
+    },
+    {
+        song: 'mãi mãi sẽ hết vào ngày mai',
+        singer: 'andiez',
+        avatar: './images/63.jpg',
+        mp3: './music/63.mp3'
+    },
+]
+
+//1. thêm số 0 vào đằng trước các số 0 đến 9
+function addZero(number){
+    if(number >= 0 && number <= 9) return `0${number}`
+    else return number
+}
+
+//2. chuyển đổi giây thành phút và format lại 
+function convertSecondsToMinutes(timeType, element = ''){
+    let seconds = 0
+    if(timeType === 'currentTime'){
+        seconds = element.currentTime.toFixed(0)
+    }else if(timeType === 'duration'){
+        seconds = element.duration.toFixed(0)
+    }else{
+        seconds = timeType.toFixed(0)
+    }
+    const minutes = Math.trunc(seconds / 60)
+    return  `${addZero(minutes)}:${addZero(seconds - minutes * 60)}`
+}
+
+//3. tối ưu việc random để 1 bài hát không lặp lại cho đến đã phát hết danh sách bài hát
+let random = []
+function randomOptimization(arr){
+    const max = arr.length
+    let numberRandom = Math.floor(Math.random() * max)
+    
+    if(random.length >= arr.length){
+        random = []
+    } 
+        
+
+    while(random.find(num => numberRandom == num) !== undefined){
+        numberRandom = Math.floor(Math.random() * max)
+    }
+
+    random.push(numberRandom)
+    return numberRandom
+}
+
+//4 lấy ra phần trăm chiều ngang của chuột và timeline
+function percentTimeLine(e){
+    let percent = ''
+    const left = timeLine.offsetLeft
+    const width = timeLine.offsetWidth
+    if(clickDown && e.clientX >= left && e.clientX <= left + width){
+        percent = (e.clientX - left) / width
+    }else if(clickDown && e.clientX < left){
+        percent = 0
+    }else if(clickDown && e.clientX > left + width){
+        percent = 1
+    }
+    
+    return percent
+}
+
+//
+
+function playAudio() {
+    if (music.paused) {
+    music.play();
+    pButton.className = "";
+    pButton.className = "pause";
+    
+    /* Tell Analytics about longSession */
+    var anc=false;
+    window.setInterval(function(){
+        if (!anc) { ga('send','event','longSession15','longSession15'); anc=true; }
+        }, 900000);
+    
+    } else {
+    music.pause();
+    pButton.className = "";
+    pButton.className = "play";
+    }}
+    function setVolume(volume){music.volume = volume;}
+
+//
+function intervalCurrent(){
+    playingTimeLeft.innerHTML = convertSecondsToMinutes('currentTime', document.querySelector('audio.active'))
+    circle.style.left = document.querySelector('audio.active').currentTime / document.querySelector('audio.active').duration * 100 + '%'
+    percentLine.style.width = document.querySelector('audio.active').currentTime / document.querySelector('audio.active').duration * 100 + '%'
+}
+
+const playList = document.getElementById('playlist')
+
+//5. render danh sách phát
+data.forEach((item, index) => {
+    playList.insertAdjacentHTML('beforeend', `
+    
+    <div class="item" data-index="${index}">
+    <audio class="audio${++index}" src="${item.mp3}"></audio>
+    <span class="item__number">${addZero(index)}</span>
+    <img src="${item.avatar}">
+    <span class="oi item__icon" data-glyph="caret-right"></span>
+    <div class="music">
+    <div class="item__song">${item.song}</div>
+    <div class="item__singer">${item.singer}</div>
+    </div>
+    <span class="item__time">00:00</span>
+    </div>
+    `)
+});
+
+//6. hiển thị thời gian bài hát của phần danh sách sau khi render xong
+document.querySelectorAll('audio').forEach(audio => {
+    audio.addEventListener('loadedmetadata', function(){
+        audio.parentElement.querySelector('.item__time').innerHTML = convertSecondsToMinutes('duration', audio)
+    })
+})
+
+ 
+const listAudio = document.querySelectorAll('audio')
+const listItem = document.querySelectorAll('.item')
+const playingImg = document.querySelector('#playing .thumbnail img')
+const thumbnail = document.querySelector('#playing .thumbnail')
+const playingSong = document.querySelector('#playing .song')
+const playingSinger = document.querySelector('#playing .singer')
+const playingTimeRight = document.querySelector('#playing .time .right')
+const playingTimeLeft = document.querySelector('#playing .time .left')
+const play = document.querySelector('.controls .play')
+const pause = document.querySelector('.controls .pause')
+const forWard = document.querySelector('.controls .forward')
+const backward = document.querySelector('.controls .backward')
+const timeLine = document.querySelector('#playing .time-line')
+const circle = timeLine.querySelector('.circle')
+const percentLine = timeLine.querySelector('.percent')
+const loop = document.querySelector('.controls .loop')
+const randomControl = document.querySelector('.controls .random')
+const music = document.querySelector('audioplayer')
+
+
+listItem[0].classList.add('active')
+listAudio[0].classList.add('active')
+
+let currentTime = ''
+
+//7. khi nhấn bài bài hát
+listItem.forEach(item => {
+    item.addEventListener('click', function(){
+
+        thumbnail.classList.remove('pause')
+        thumbnail.classList.remove('play')
+        
+        //6.1 hiệu ứng active 
+        document.querySelector('.item.active').classList.remove('active')
+        item.classList.add('active')
+
+        //6.2 dừng audio đang phát vào đặt thời gian về 0
+        document.querySelector('audio.active').pause()
+        document.querySelector('audio.active').currentTime = 0
+
+        //6.3 xóa, thêm class active cho audio
+        document.querySelector('audio.active').classList.remove('active')
+        item.querySelector('audio').classList.add('active')
+        
+        //6.4 hiệu ứng thay đổi ảnh, bài hát, ca sĩ, thời gian
+        playingImg.setAttribute('src', item.querySelector('img').getAttribute('src'))
+        playingSong.innerHTML = item.querySelector('.item__song').innerHTML
+        playingSinger.innerHTML = item.querySelector('.item__singer').innerHTML
+        playingTimeRight.innerHTML = item.querySelector('.item__time').innerHTML
+
+        //6.5 hiệu ứng cuộn, clearInterval, xóa class active của loop
+        item.scrollIntoView({behavior: "smooth", block: "center"})
+        clearInterval(currentTime)
+        loop.classList.remove('active')
+
+        play.click()
+    })
+})
+
+//8. khi ấn play
+play.addEventListener('click', function(){
+    forWard.style.pointerEvents = 'all'
+
+    thumbnail.classList.remove('pause')
+    thumbnail.classList.add('play')
+
+    //8.1 hiệu ứng đổi sang pause
+    play.style.display = 'none'
+    pause.style.display = 'block'
+    
+    //8.2 phát audio đang active
+    document.querySelector('audio.active').play()
+    
+    //8.3 hiệu ứng thời giạn hiện tại của bài hát
+    currentTime =  setInterval(()=>{
+        intervalCurrent()
+    }, 100)
+
+    //8.4 khi audio active kết thúc
+    document.querySelector('audio.active').onended = function(){
+        clearInterval(currentTime)
+        
+        // nếu ấn chọn loop
+        if(loop.classList.contains('active')){
+            play.click()
+        // mặc định phát bài tiếp theo
+        }else{
+            forWard.click()
+        }
+    }
+    
+
+})
+
+//9. khi ấn pause
+pause.addEventListener('click', function(){
+    pause.style.display = 'none'
+    play.style.display = 'block'
+
+    thumbnail.classList.remove('play')
+    thumbnail.classList.add('pause')
+
+    clearInterval(currentTime)
+    document.querySelector('audio.active').pause()
+})
+
+//10. khi ấn forWard
+forWard.addEventListener('click', function(){
+    // nếu ấn chọn random
+    if(randomControl.classList.contains('active'))
+    {
+        listItem[randomOptimization(listItem)].click()
+    }
+    else
+    {
+        const nextItem = document.querySelector('.item.active').nextElementSibling
+        if(nextItem){
+            nextItem.click()
+        }else{
+            pause.click()
+            this.style.pointerEvents = 'none'
+        }
+    }
+})
+
+//11. khi ấn backWard
+backward.addEventListener('click', function(){
+    if(randomControl.classList.contains('active'))
+    {
+        if(document.querySelector('audio.active').currentTime <= 3){
+            listItem[randomOptimization(listItem)].click()
+        }else{
+            document.querySelector('audio.active').currentTime = 0
+        }
+    }
+    else
+    {
+        const prevItem = document.querySelector('.item.active').previousElementSibling
+        if(prevItem && document.querySelector('audio.active').currentTime <= 3){
+            prevItem.click()
+        }else{
+            document.querySelector('audio.active').currentTime = 0
+        }
+    }
+    
+})
+
+let clickDown = false
+
+//12. hiệu ứng kéo thả time line
+window.addEventListener('mousedown', function(e){
+    if(e.target.matches('.circle') || e.target.matches('.time-line') || e.target.matches('.percent')){
+        clickDown = true
+        clearInterval(currentTime)
+    }
+})
+
+window.addEventListener('mousemove', function(e){
+    this.document.body.style.userSelect = 'none'
+
+    let percent = percentTimeLine(e)
+
+    if(clickDown){
+        clearInterval(currentTime)
+        playingTimeLeft.innerHTML =  convertSecondsToMinutes(percent * document.querySelector('audio.active').duration)
+        circle.style.left = percent * 100 + '%'
+        percentLine.style.width = percent * 100 + '%'
+    }
+    
+})
+
+window.addEventListener('mouseup', function(e){
+    if(clickDown){
+        document.querySelector('audio.active').currentTime = percentTimeLine(e) * document.querySelector('audio.active').duration
+        currentTime =  setInterval(()=>{
+            intervalCurrent()
+        }, 100)
+        clickDown = false
+    }
+})
+
+timeLine.addEventListener('click', function(){
+    intervalCurrent()
+})
+
+
+//13. khi ấn vào loop
+loop.addEventListener('click', function(){
+    randomControl.classList.remove('active')
+    this.classList.toggle('active')
+})
+
+//14. khi ấn vào random
+randomControl.addEventListener('click', function(){
+    loop.classList.remove('active')
+    this.classList.toggle('active')
+    random = []
+    if(this.classList.contains('active')){
+        random.push(document.querySelector('.item.active').dataset.index)
+    }
+})
+
+thumbnail.addEventListener('animationend', function(e){
+    if(e.animationName === 'rotatePause'){
+        thumbnail.classList.remove('pause')
+        this.style.borderRadius = '20px'
+    }
+})
